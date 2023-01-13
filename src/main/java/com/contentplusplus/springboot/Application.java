997632ef4c67cf9c3e6652eb98b2052d0756c7b5
Ajay Kumar <Ajay Kumar@DESKTOP-2EDS8Q5.attlocal.net>
2023-01-12 23:39:51 -0500

    update

--- src/main/java/com/contentplusplus/springboot/Application.java
+++ src/main/java/com/contentplusplus/springboot/Application.java
@@ -8,6 +8,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println(
+				"\n=========DEFAULT CREDENTIALS=========\nIf used dbscript_mysql.sql then....\nUsername:admin@admin\nPassword:admin@admin\n=====================================\n");
 	}
 
 }
